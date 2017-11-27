6da18703d9606ea299dd11010aced76024efa2e3
jroslindo <jroslindotab@gmail.com>
2017-11-27 09:27:24 -0200

    Agora com data

--- RecycleViewExercise/app/src/main/java/joaopauloroslindo/recycleviewexercise/EscolheData.java
+++ RecycleViewExercise/app/src/main/java/joaopauloroslindo/recycleviewexercise/EscolheData.java
@@ -1,13 +1,32 @@
 package joaopauloroslindo.recycleviewexercise;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.text.SimpleDateFormat;
 
 public class EscolheData extends AppCompatActivity {
-
+private DatePicker dataDoCalendario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escolhe_data);
+    }
+
+    public void retorna (View quemclicou){
+        dataDoCalendario = (DatePicker) findViewById(R.id.dpData);
+        String Data_retornado = new String();
+        Data_retornado = ""+ dataDoCalendario.getDayOfMonth() +"/" + (dataDoCalendario.getMonth()+1) + "/" + dataDoCalendario.getYear() ;
+
+        Intent intencao = new Intent(this, NovoAbastecimento.class);
+        intencao.putExtra("Data_Retornado", Data_retornado);
+        setResult(Activity.RESULT_OK, intencao);
+        finish();
+
     }
 }
--- RecycleViewExercise/app/src/main/java/joaopauloroslindo/recycleviewexercise/NovoAbastecimento.java
+++ RecycleViewExercise/app/src/main/java/joaopauloroslindo/recycleviewexercise/NovoAbastecimento.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class NovoAbastecimento extends AppCompatActivity {
 
@@ -21,10 +25,18 @@ public class NovoAbastecimento extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+        String BotaNaPorra = data.getStringExtra("Data_Retornado");
+        EditText aPorra = (EditText) findViewById(R.id.etData);
+        aPorra.setText(BotaNaPorra);
+
+    }
 
     public void VaiParaEscolhaAData(View quemclicou) {
         Intent intencao = new Intent(getApplicationContext(), EscolheData.class);
+        String DataRetornado = new String();
 
-        startActivity(intencao);
+        intencao.putExtra("DataRetorno", DataRetornado);
+        startActivityForResult(intencao, 1);
     }
 }
